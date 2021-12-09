2ed58a37954d4c55d012c62dc8a227a12a5f7b50
agildw <yudistiraagil24@gmail.com>
2021-12-09 21:59:05 +0700

    add tiktok downloader with watermark

--- index.js
+++ index.js
@@ -1,28 +1,52 @@
 const { Telegraf } = require('telegraf');
 const fs = require('fs');
 require('dotenv').config();
+
 const tiktok = require('./tiktok');
+const tiktokwm = require('./tiktokwm');
 
 //your bot token
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.start((ctx) => {
-    ctx.reply('/tiktok tiktok downloader using watermark')
+    ctx.reply('/tiktok tiktok downloader without watermark\n/tiktokwm tiktok downloader with watermark')
 });
 
 bot.command('/tiktok', (ctx) => {
     const getText = ctx.update.message.text.split(' ');
     const tiktokUrl = getText[1];
 
-    tiktok(tiktokUrl)
-        .then((response) => {
-            ctx.replyWithVideo(response.data.aweme_details[0].video.bit_rate[0].play_addr.url_list[2])
-        })
-        .catch((error) => {
-            console.log(error)
-        })
+    if (tiktokUrl !== undefined) {
+        tiktok(tiktokUrl)
+            .then((response) => {
+                ctx.replyWithVideo(response.data.aweme_details[0].video.bit_rate[0].play_addr.url_list[2])
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    } else {
+        ctx.reply('*invalid command*\n/tiktok video_link', { parse_mode: 'MarkdownV2' })
+    }
+
 })
 
+bot.command('/tiktokwm', (ctx) => {
+    const getText = ctx.update.message.text.split(' ');
+    const tiktokUrl = getText[1];
+
+    if (tiktokUrl !== undefined) {
+        tiktokwm(tiktokUrl)
+            .then((response) => {
+                // console.log(response.data.aweme_details[0].video)
+                ctx.replyWithVideo(response.data.aweme_details[0].video.download_addr.url_list[0])
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    } else {
+        ctx.reply('*invalid command*\n/tiktokwm video_link', { parse_mode: 'MarkdownV2' })
+    }
+})
 
 
 bot.launch();
---
+++ tiktokwm.js
@@ -0,0 +1,20 @@
+const axios = require('axios');
+
+module.exports = function tiktokwm(url) {
+    const config = {
+        url: 'https://masgimenz.my.id/tiktok/',
+        method: 'GET',
+        params: {
+            url: url
+        }
+    }
+    return new Promise((resolve, reject) => {
+        axios(config)
+            .then((response) => {
+                resolve(response);
+            })
+            .catch((error) => {
+                reject(error)
+            })
+    })
+}
